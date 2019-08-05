61e1647334e5b77f35af700d5c2648e7c2324b61
Rabbit <yfhj1990@hotmail.com>
2019-08-05 17:17:29 +0800

    Closed #2

--- src/generate.ts
+++ src/generate.ts
@@ -4,20 +4,24 @@ import prompts from 'prompts'
 
 import { BinFile } from './createBinFile'
 
-export interface Options {
+export interface GenerateOptions {
   dryrun: boolean
   silent: boolean
   yes: boolean
+  root: string
+  ts: boolean
 }
 
-const DEFAULT_OPTIONS: Options = {
+const DEFAULT_GENERATE_OPTIONS: GenerateOptions = {
   dryrun: false,
   silent: false,
-  yes: false
+  yes: false,
+  root: '.',
+  ts: false
 }
 
-export async function generate(binFile: BinFile, options: Partial<Options> = {}) {
-  const opts: Options = { ...DEFAULT_OPTIONS, ...options }
+export async function generate(binFile: BinFile, options: Partial<GenerateOptions> = {}) {
+  const opts: GenerateOptions = { ...DEFAULT_GENERATE_OPTIONS, ...options }
   const {
     dryrun,
     silent,
@@ -26,7 +30,7 @@ export async function generate(binFile: BinFile, options: Partial<Options> = {})
 
   const result: string = render(binFile)
 
-  if(!silent) report(binFile, result)
+  if(!silent) report(binFile, result, opts)
   if(dryrun) return
 
   if(!silent && !yes) {
@@ -56,8 +60,13 @@ require('${binFile.script}')['${binFile.export}'](${binFile.args})
 `
 }
 
-function report(binFile: BinFile, result: string): void {
+function report(binFile: BinFile, result: string, options: GenerateOptions): void {
   console.log(`\n[bin-file-gen] Summary:`)
+  console.log()
+  console.log(`  - Root:     ${options.root}`)
+  console.log(`  - TSConfig: ${options.ts}`)
+  console.log(`  - DryRun:   ${options.dryrun}`)
+  console.log(`  - Ask:      ${!options.yes}`)
   console.log()
   console.log(`  - Name:     ${binFile.name}`)
   console.log(`  - File:     ${binFile.file}`)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import readPkgUp from 'read-pkg-up'
-import { generate, Options as GenerateOptions } from './generate'
+import { generate, GenerateOptions } from './generate'
 import resolvePkgConfig from './resolvePkgConfig'
 import resolveTsConfig from './resolveTsConfig'
 import createBinFile, { BinFile } from './createBinFile'
@@ -16,10 +16,10 @@ const DEFAULT_PROJECT: string = '.'
 const DEFAULT_SCRIPT: string = 'cli'
 
 function getOutputAndScript(project: string, output: string, script: string): [ string, string ] {
-  const absoluteProjectPath: string = path.resolve(project)
-  const absoluteFilePath: string = path.isAbsolute(output) ? output : path.resolve(absoluteProjectPath, output)
-  const dir: string = path.dirname(absoluteFilePath)
-  const relativePath: string = path.relative(dir, absoluteProjectPath)
+  const absoluteProjectPath = path.resolve(project)
+  const absoluteFilePath = path.isAbsolute(output) ? output : path.resolve(absoluteProjectPath, output)
+  const dir = path.dirname(absoluteFilePath)
+  const relativePath = path.relative(dir, absoluteProjectPath)
   return [ absoluteFilePath, path.join(relativePath, script).replace(/\\/g, '/') ]
 }
 
@@ -28,15 +28,18 @@ async function main(options: Partial<Options> = {}): Promise<void> {
   if(undefined === result) throw makePackageJSONNotFoundError()
   const root = path.dirname(result.path)
   const name = getPackageName(result.package.name)
-  const project: string = options.project || resolveTsConfig(root, DEFAULT_PROJECT)
-  const file: string = options.output || DEFAULT_OUTPUT + '/' + name
-  const [ output, script ]: [ string, string ] = getOutputAndScript(project, file, options.script || DEFAULT_SCRIPT)
-  const { name: fileName, path: filePath }: NameResolveResult = resolvePkgConfig(result.package, output, project, { report: !options.silent })
-  const binFile: BinFile = createBinFile(fileName, filePath, script)
+  const ts = resolveTsConfig(root, DEFAULT_PROJECT)
+  const project = options.project || ts
+  const file = options.output || DEFAULT_OUTPUT + '/' + name
+  const [ output, script ] = getOutputAndScript(project, file, options.script || DEFAULT_SCRIPT)
+  const { name: fileName, path: filePath } = resolvePkgConfig(result.package, output, project, { report: !options.silent })
+  const binFile = createBinFile(fileName, filePath, script)
   
   generate(binFile, {
-    silent: options.silent,
-    dryrun: options.dryrun
+    silent: Boolean(options.silent),
+    dryrun: Boolean(options.dryrun),
+    root,
+    ts: Boolean(ts)
   })
 }
 
